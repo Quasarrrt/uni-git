e286dc9433adaaebfc2fa6aa097b429b53527c6e
Quasarrrt <emperortaika@gmail.com>
2022-02-22 00:13:41 +0300

    add album component

---
+++ album.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from 'album.module.css'
+const Album = () => {
+  return (
+    <div className={styles.section}>
+      Album
+    </div>
+  );
+};
+
+export default Album;
